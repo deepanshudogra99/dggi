01962d5cc9eabafcbe399c0152bac25abe5a0ed4
Deepanshu Dogra <dogra.deepanshu99@gmail.com>
2024-04-24 02:32:59 +0530

    changes to welocme.blade.php

--- resources/views/components/welcome.blade.php
+++ resources/views/components/welcome.blade.php
@@ -1,7 +1,7 @@
-<div class="p-6 lg:p-8 bg-white dark:bg-gray-800 dark:bg-gradient-to-bl dark:from-gray-700/50 dark:via-transparent border-b border-gray-200 dark:border-gray-700">
+<div style="text-align: center;" class="p-6 lg:p-8 bg-white dark:bg-gray-800 dark:bg-gradient-to-bl dark:from-gray-700/50 dark:via-transparent border-b border-gray-200 dark:border-gray-700">
     <x-application-logo class="block h-12 w-auto" />
 
-    <h1 class="mt-8 text-2xl font-medium text-gray-900 dark:text-white">
+    <h1 class="mt-8 text-2xl font-medium text-green-900 dark:text-white">
         Welcome to <b><u> District Good Governance Index </u></b> 
     </h1>
 
@@ -10,7 +10,7 @@
     </p>
 </div>
 
-<div class="bg-gray-200 dark:bg-gray-800 bg-opacity-25 grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8 p-6 lg:p-8">
+<div class="mt-10 bg-gray-200 dark:bg-gray-800 bg-opacity-25 grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8 p-6 lg:p-8">
     <!-- <div>
         <div class="flex items-center">
           
