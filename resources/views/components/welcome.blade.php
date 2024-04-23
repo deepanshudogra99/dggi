53db087a9a1feacaaecffe8fb8af221a8917723b
Deepanshu Dogra <dogra.deepanshu99@gmail.com>
2024-04-24 02:03:53 +0530

    front end changes

--- resources/views/components/welcome.blade.php
+++ resources/views/components/welcome.blade.php
@@ -2,14 +2,11 @@
     <x-application-logo class="block h-12 w-auto" />
 
     <h1 class="mt-8 text-2xl font-medium text-gray-900 dark:text-white">
-        Welcome to your Jetstream application!
+        Welcome to <b><u> District Good Governance Index </u></b> 
     </h1>
 
     <p class="mt-6 text-gray-500 dark:text-gray-400 leading-relaxed">
-        Laravel Jetstream provides a beautiful, robust starting point for your next Laravel application. Laravel is designed
-        to help you build your application using a development environment that is simple, powerful, and enjoyable. We believe
-        you should love expressing your creativity through programming, so we have spent time carefully crafting the Laravel
-        ecosystem to be a breath of fresh air. We hope you love it.
+    A well-structured DGGI provides valuable insights into the strengths and weaknesses of governance mechanisms within a district, helping policymakers and administrators identify areas for improvement and implement targeted interventions. By measuring and tracking progress over time, the index facilitates evidence-based decision-making, fosters healthy competition among districts, and promotes the overall development and well-being of communities.
     </p>
 </div>
 
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,8 +1,16 @@
 <x-app-layout>
-    <x-slot name="header">
+    <!-- <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('DGGI') }}
         </h2>
+    </x-slot> -->
+    <x-slot name="header">
+        <h2 class="font-semibold text-3xl text-green-900 leading-tight">
+            {{ __('DGGI') }}
+        </h2>
+        <h3 class="font-semibold text-xl text-green-900 leading-tight">
+            (Himachal Pradesh)
+        </h3>
     </x-slot>
 
     <div class="py-12">
