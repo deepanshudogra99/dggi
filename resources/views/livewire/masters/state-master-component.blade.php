f95e4781a2d505d7ab230018e420d00d60e19f32
Deepanshu Dogra <dogra.deepanshu99@gmail.com>
2024-04-26 00:13:40 +0530

    added state abbrevation

--- resources/views/livewire/masters/state-master-component.blade.php
+++ resources/views/livewire/masters/state-master-component.blade.php
@@ -21,6 +21,10 @@
                             <x-label for="statename" name="statename" class="block text-sm font-medium text-gray-700"/><b>State Name</b>
                             <x-input wire:model="statename" name="statename" type="text" required class="mt-2 p-2 block w-full border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm" placeholder="Enter state name" />
                         </div>    
+                        <div class="mb-4">
+                            <x-label for="stateabr" name="stateabr" class="block text-sm font-medium text-gray-700"/><b>State Abbrevation</b>
+                            <x-input wire:model="stateabr" name="stateabr" type="text" required class="mt-2 p-2 block w-full border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm" placeholder="Enter state abbrevation    " />
+                        </div>    
                     </form>
             </div>
         </x-slot>
